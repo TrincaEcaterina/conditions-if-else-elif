11aac4ac6bccc6fdf1419bae8605345dbc993421
TrincaEcaterina <108815009+TrincaEcaterina@users.noreply.github.com>
2022-07-10 19:37:35 +0300

    break the cycle

---
+++ BOOYA_while.py
@@ -0,0 +1,13 @@
+# Добавьте недостающий фрагмент кода, который остановит цикл только тогда, 
+# когда введенный пользователем ответ (answer) будет равен "yes!"
+
+while True:
+    print ("BOO YA!")
+    answer=input("Do you wan't me to stop ?")
+    if answer=='Yes':
+        break
+print('Your "BOO YA!" is finish!')
+
+        
+
+    
